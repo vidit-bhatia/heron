b78b4ed9241b5ac3db82beaafaed3047e904f8b6
Maosong Fu <mfu@twitter.com>
2016-08-30 15:15:43 -0700

    fix memory leak as master

--- heron/stmgr/src/cpp/manager/stmgr-server.cpp
+++ heron/stmgr/src/cpp/manager/stmgr-server.cpp
@@ -530,6 +530,8 @@ void StMgrServer::HandleStartBackPressureMessage(Connection* _conn,
     LOG(ERROR) << "Received start back pressure message from unknown stream manager "
                << _message->topology_name() << " " << _message->topology_id() << " "
                << _message->stmgr() << " " << _message->message_id();
+
+    release(_message);
     return;
   }
   CHECK(rstmgrs_.find(_conn) != rstmgrs_.end());
@@ -537,6 +539,8 @@ void StMgrServer::HandleStartBackPressureMessage(Connection* _conn,
   stmgrs_who_announced_back_pressure_.insert(stmgr_id);
 
   StartBackPressureOnSpouts();
+
+  release(_message);
 }
 
 void StMgrServer::HandleStopBackPressureMessage(Connection* _conn,
@@ -546,6 +550,8 @@ void StMgrServer::HandleStopBackPressureMessage(Connection* _conn,
     LOG(ERROR) << "Received stop back pressure message from unknown stream manager "
                << _message->topology_name() << " " << _message->topology_id() << " "
                << _message->stmgr();
+
+    release(_message);
     return;
   }
   CHECK(rstmgrs_.find(_conn) != rstmgrs_.end());
@@ -557,6 +563,8 @@ void StMgrServer::HandleStopBackPressureMessage(Connection* _conn,
     stmgrs_who_announced_back_pressure_.erase(stmgr_id);
     AttemptStopBackPressureFromSpouts();
   }
+
+  release(_message);
 }
 
 void StMgrServer::SendStartBackPressureToOtherStMgrs() {
